c8e6f9e20b957e52d1ddf5d5e0416f7f04d7b12b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2007-01-18 18:36:15 +0000

    Releasing 0.0.3

--- lib/win32screenshot.rb
+++ lib/win32screenshot.rb
@@ -42,6 +42,21 @@ module Win32
     extern "long ReleaseDC(long, HDC)"
     extern "BOOL SetForegroundWindow(HWND)"
 
+    # Callback defined once. It has to be defined only once in order to avoid the error "Too many callbacks"
+    FIND_WINDOW_CALLBACK = DL.callback('ILL') do |curr_hwnd, p|
+      textLength, a = GetWindowTextLength.call(curr_hwnd)
+      captionBuffer = " " * (textLength+1)
+      t, textCaption = GetWindowText.call(curr_hwnd, captionBuffer, textLength+1)    
+      text = textCaption[1].to_s
+      # TODO: this is very ugly. How do we pass title query as a param to this block?
+      if text =~ $win32screenshot_title_query
+        $win32screenshot_hwnd_ref[0] = curr_hwnd
+        0
+      else
+        1
+      end
+    end
+
     module_function
     def capture(hScreenDC, x1, y1, x2, y2)
       w = x2-x1
@@ -104,27 +119,28 @@ module Win32
     end
 
     module_function
-    def window(title_query, delay=0.1)
-      hwnd = nil
-
-      proc = DL.callback('ILL') do |curr_hwnd, lparam|
-        textLength, a = GetWindowTextLength.call(curr_hwnd)
-        captionBuffer = " " * (textLength+1)
-        t, textCaption = GetWindowText.call(curr_hwnd, captionBuffer, textLength+1)    
-        text = textCaption[1].to_s
-        if text =~ title_query
-          hwnd = curr_hwnd
-          0
-        else
-          1
-        end
-      end
-      EnumWindows.call(proc, 0)
-
-      raise "Couldn't find window with title matching #{title_query}" if hwnd.nil?
-      setForegroundWindow(hwnd)
-      sleep(delay)
+    def window(title_query, pause=0.1)
+      hwnd = set_foreground_window(title_query)
+      sleep(pause)
       capture_hwnd(hwnd)
+    end
+
+    module_function
+    def set_foreground_window(title_query)
+      hwnd = get_hwnd(title_query)
+      setForegroundWindow(hwnd)
+      hwnd
+    end
+    
+    module_function
+    def get_hwnd(title_query)
+      # TODO: ugly, yes I know, but how do we pass in args and get return values from DL callbacks??
+      $win32screenshot_hwnd_ref = []
+      $win32screenshot_title_query = title_query
+      EnumWindows.call(FIND_WINDOW_CALLBACK, 0)
+      hwnd = $win32screenshot_hwnd_ref[0]
+      raise "Couldn't find window with title matching #{title_query}" if hwnd.nil?
+      hwnd
     end
   end
 end
--- test/win32screenshot_test.rb
+++ test/win32screenshot_test.rb
@@ -3,31 +3,32 @@ require 'rubygems'
 require 'RMagick'
 
 # Prereqs for this test:
-# * Must be run from a command window with dimensions 1600x1200 pixels (160x60 characters, default font)
-# * Screen res must be 1600x1200
-# * Must have a maximised Firefox open
+# * Screen res must be 1280x1024
+# * Must be run from a command window with dimensions 150x60 characters and default font
+# * Must have a maximised IE open
 #
 class Win32screenshotTest < Test::Unit::TestCase
 
   def test_should_capture_foreground
     width, height, bmp = Win32::Screenshot.foreground
-    assert_equal 1280, width
-    assert_equal 720,  height
+    assert_equal 1200, width
+    assert_equal 744,  height
     assert_image(bmp)
   end
   
   def test_should_capture_desktop
     width, height, bmp = Win32::Screenshot.desktop
-    assert_equal 1600, width
-    assert_equal 1200,  height
+    assert_equal 1280, width
+    assert_equal 1024,  height
     assert_image(bmp)
   end
 
-  def test_should_capture_window_by_title
-    width, height, bmp = Win32::Screenshot.window(/Firefox/)
-    assert_equal 1600, width
-    assert_equal 1147,  height
-    assert_image(bmp, "ff.png")
+  def test_should_set_foreground_window_by_title
+    width, height, bmp = Win32::Screenshot.window(/Internet Explorer/)
+    sleep(0.2)
+    assert_image(bmp)
+    assert_equal 1280, width
+    assert_equal 975,  height
   end
   
 
