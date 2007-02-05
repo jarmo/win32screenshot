a7320aa0abc86f12016ec18fed1d8b11d2697e62
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2007-02-05 09:34:25 +0000

    Add test to address image corruption

--- test/win32screenshot_test.rb
+++ test/win32screenshot_test.rb
@@ -25,20 +25,26 @@ class Win32screenshotTest < Test::Unit::TestCase
 
   def test_should_set_foreground_window_by_title
     width, height, bmp = Win32::Screenshot.window(/Internet Explorer/)
-    sleep(0.2)
     assert_image(bmp)
     assert_equal 1280, width
     assert_equal 975,  height
   end
   
+  def test_should_capture_small_windows_without_corrupting_image
+    width, height, bmp = Win32::Screenshot.window(/Firefox/)
+    assert_image(bmp, 'ff')
+    assert_equal 1280, width
+    assert_equal 975,  height
+  end
 
   def assert_image(bmp, file=nil)
+    File.open("#{file}.bmp", "wb") {|io| io.write(bmp)} unless file.nil?
     assert_equal 'BM',  bmp[0..1]
     img = Magick::Image.from_blob(bmp)
     png = img[0].to_blob do
       self.format = 'PNG'
     end
     assert_equal "\211PNG",  png[0..3]
-    File.open(file, "wb") {|io| io.write(png)} unless file.nil?
+    File.open("#{file}.png", "wb") {|io| io.write(png)} unless file.nil?
   end
 end
