68359429988dd01a3d7b90608209210cc5aad15d
Jarmo Pertman <jarmo.p@gmail.com>
2010-07-01 23:38:05 +0300

    Added method window_area for capturing specified area of the window instead of full window. Removed usage of ShowWindow with parameter SW_SHOW when trying to bring window to front due it's behaviour of resizing window if it was maximized.

--- lib/win32/screenshot.rb
+++ lib/win32/screenshot.rb
@@ -8,27 +8,56 @@ module Win32
       # captures foreground
       def foreground(&proc)
         hwnd = BitmapMaker.foreground_window
-        BitmapMaker.capture(hwnd, &proc)
+        BitmapMaker.capture_all(hwnd, &proc)
       end
 
       # captures desktop
       def desktop(&proc)
         hwnd = BitmapMaker.desktop_window
-        BitmapMaker.capture(hwnd, &proc)
+        BitmapMaker.capture_all(hwnd, &proc)
       end
 
       # captures window with a *title_query* and waits *pause* (by default is 0.5)
       # seconds after trying to set window to the foreground
       def window(title_query, pause=0.5, &proc)
-        hwnd = BitmapMaker.hwnd(title_query)
-        raise "window with title '#{title_query}' was not found!" unless hwnd
+        hwnd = window_hwnd(title_query)
         hwnd(hwnd, pause, &proc)
+      end
+
+      # captures area of the window with a *title_query*
+      # where *x1* and *y1* are 0 in the upper left corner and
+      # *x2* specifies the width and *y2* the height of the area to be captured
+      def window_area(title_query, x1, y1, x2, y2, pause=0.5, &proc)
+        hwnd = window_hwnd(title_query)
+        validate_coordinates(hwnd, x1, y1, x2, y2)
+        BitmapMaker.prepare_window(hwnd, pause)
+        BitmapMaker.capture_area(hwnd, x1, y1, x2, y2, &proc)
       end
 
       # captures by window handle
       def hwnd(hwnd, pause=0.5, &proc)
         BitmapMaker.prepare_window(hwnd, pause)
-        BitmapMaker.capture(hwnd, &proc)
+        BitmapMaker.capture_all(hwnd, &proc)
+      end
+
+      private
+
+      def window_hwnd(title_query)
+        hwnd = BitmapMaker.hwnd(title_query)
+        raise "window with title '#{title_query}' was not found!" unless hwnd
+        hwnd
+      end
+
+      def validate_coordinates(hwnd, *coords)
+        specified_coordinates = coords.join(', ')
+        invalid = coords.any? {|c| c < 0}
+
+        x1, y1, x2, y2 = *coords
+        invalid ||= x1 >= x2 || y1 >= y2
+
+        max_x1, max_y1, max_x2, max_y2 = BitmapMaker.dimensions_for(hwnd)
+        invalid ||= x2 > max_x2 || y2 > max_y2
+        raise "specified coordinates (#{specified_coordinates}) are invalid!" if invalid
       end
     end
 
