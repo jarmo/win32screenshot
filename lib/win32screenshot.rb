282b6bc0b233be8882423cb857df419751389225
Jarmo Pertman <jarmo.p@gmail.com>
2010-08-07 15:53:10 +0300

    removed a file used only for deprecation notices - win32screenshot.rb, make sure that ALL require statements require from now on 'win32/screenshot'

--- lib/win32screenshot.rb
+++
@@ -1,3 +0,0 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'lib'))
-Kernel.warn %q(DEPRECATION: use "require 'win32/screenshot'" instead of "require 'win32screenshot'")
-require "win32/screenshot"
