87d3a69dfcde727fce48ae8c74e0bac9ed09a797
Jarmo Pertman <jarmo.p@gmail.com>
2010-12-16 15:36:48 +0200

    added entrypoint for gem

--- lib/win32/screenshot/screenshot.rb
+++ lib/win32/screenshot/screenshot.rb
@@ -1,15 +1,3 @@
-require 'ffi'
-require 'stringio' # this is needed for MiniMagick for now
-require 'mini_magick'
-
-# environment variables for bundled MiniMagick
-ENV["PATH"] = "#{File.dirname(__FILE__) + "/../../ext"};#{ENV["PATH"]}"
-ENV["MAGICK_CODER_MODULE_PATH"] = File.dirname(__FILE__) + "/../../ext/modules/coders"
-
-require File.dirname(__FILE__) + '/screenshot/image'
-require File.dirname(__FILE__) + '/screenshot/bitmap_maker'
-require File.dirname(__FILE__) + '/util'
-
 module Win32
   # Captures screenshots with Ruby on Windows
   class Screenshot
