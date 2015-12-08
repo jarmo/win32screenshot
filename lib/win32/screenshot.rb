48ef61ddf6162ef6babf672be8d1b8f03e59665b
unknown <rcastro@rcastro.mainman.dcs>
2015-12-08 09:41:57 -0700

    update bundled ImageMagick version to 6.9.2-8-q16-x86

--- lib/win32/screenshot.rb
+++ lib/win32/screenshot.rb
@@ -8,4 +8,4 @@ require File.dirname(__FILE__) + '/screenshot/image'
 require File.dirname(__FILE__) + '/screenshot/bitmap_maker'
 
 # add bundled ImageMagick into path
-ENV["PATH"] = "#{File.dirname(__FILE__) + "/../../ext/ImageMagick-6.9.1-6-portable-Q16-x86"};#{ENV["PATH"]}"
+ENV["PATH"] = "#{File.dirname(__FILE__) + "/../../ext/ImageMagick-6.9.2-8-portable-Q16-x86"};#{ENV["PATH"]}"
